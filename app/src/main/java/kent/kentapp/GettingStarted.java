3067f41895b848df76c21275b9c201c613b7ee1e
CO600-MH520 <mh520@kent.ac.uk>
2016-01-26 18:22:54 +0000

    Updated main_menu, profile XML Created friends, getting_started, photo, social, video XML Created GettingStarted Java Class  Added Drawables

---
+++ app/src/main/java/kent/kentapp/GettingStarted.java
@@ -0,0 +1,58 @@
+package kent.kentapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.CheckBox;
+import android.widget.Button;
+import android.widget.Toast;
+import android.content.Intent;
+
+
+public class GettingStarted extends Activity implements OnClickListener {
+
+    CheckBox it_account_checkbox, fees_checkbox, accommodation_checkbox, immunisation_checkbox, wellbeing_checkbox;
+    Button continueButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_getting_started);
+
+
+        it_account_checkbox = (CheckBox) findViewById(R.id.it_account_checkbox);
+        fees_checkbox = (CheckBox) findViewById(R.id.fees_checkbox);
+        accommodation_checkbox = (CheckBox) findViewById(R.id.accommodation_checkbox);
+        immunisation_checkbox = (CheckBox) findViewById(R.id.immunisation_checkbox);
+        wellbeing_checkbox = (CheckBox) findViewById(R.id.optional_check);
+
+        continueButton = (Button) findViewById(R.id.proceed_button);
+        continueButton.setOnClickListener(this);
+    }
+
+    public void onClick(View v) {
+
+        Intent proceed = new Intent(GettingStarted.this, MainMenu.class);
+
+        if (continueButton.isPressed()) {
+            if (it_account_checkbox.isChecked() && fees_checkbox.isChecked() && accommodation_checkbox.isChecked()
+                    && immunisation_checkbox.isChecked()) {
+
+                startActivity(proceed);
+
+            }
+        }
+        else
+
+        {
+            showToast();
+        }
+    }
+
+    public void showToast() {
+        Toast.makeText(this, "Please complete all previous tasks before continuing", Toast.LENGTH_LONG).show();
+    }
+
+}
+
--- app/src/main/java/kent/kentapp/MainMenu.java
+++ app/src/main/java/kent/kentapp/MainMenu.java
@@ -10,8 +10,8 @@ public class MainMenu extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_menu);
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main_menu);
 
         final Button btnMap = (Button) findViewById(R.id.btnMap);
         btnMap.setOnClickListener(new View.OnClickListener() {
