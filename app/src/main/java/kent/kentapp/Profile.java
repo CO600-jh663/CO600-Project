7fa596471cdaeaa77bc5194729fd5e14a87492c4
CO600-MH520 <mh520@kent.ac.uk>
2016-03-09 23:57:01 +0000

    Changes to: Java Profile -> ability to upload profile picture

--- app/src/main/java/kent/kentapp/Profile.java
+++ app/src/main/java/kent/kentapp/Profile.java
@@ -1,13 +1,25 @@
 package kent.kentapp;
 
+
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.net.Uri;
+import android.provider.MediaStore;
+
+
 
 public class Profile extends AppCompatActivity {
+
+
+    ImageButton profilePicture = (ImageButton) findViewById(R.id.profile_picture);
+    private static int RESULT_LOAD_IMAGE = 1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,6 +71,15 @@ public class Profile extends AppCompatActivity {
             }
         });
 
+        profilePicture.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(
+                        Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+                startActivityForResult(intent, RESULT_LOAD_IMAGE);
+            }
+        });
+
         final Button btnSDS = (Button) findViewById(R.id.btnSDS);
         btnSDS.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -78,5 +99,26 @@ public class Profile extends AppCompatActivity {
         });
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = {MediaStore.Images.Media.DATA};
+
+            Cursor cursor = getContentResolver().query(selectedImage,
+                    filePathColumn, null, null, null);
+            cursor.moveToFirst();
+
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String picturePath = cursor.getString(columnIndex);
+            cursor.close();
+
+            profilePicture.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+        }
+    }
 }
+
+
 
